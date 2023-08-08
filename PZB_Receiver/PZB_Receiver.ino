91456ea0cbbed5f43afb9691ccb48e573e36668e
xXDesolatorXx <80487483+xXDesolatorXx@users.noreply.github.com>
2023-08-08 21:20:09 +0200

    Update PZB_Receiver.ino
    updated from V0.1 to V0.2
    added support for an RGB LED for PIN 21,22 and 23
    not used in first series of five produced (A001 to A005)

--- PZB_Receiver/PZB_Receiver.ino
+++ PZB_Receiver/PZB_Receiver.ino
@@ -1,5 +1,8 @@
 /*
-PZB Receiver 0.1
+PZB Receiver 0.2
+
+PCB Prototype Version for Akiem Technik
+
 Made by Fabian Witzel
 */
 
@@ -24,38 +27,57 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.print("Int: ");
 
 if (myData.a == 500){
-    digitalWrite(18,HIGH);
+    digitalWrite(17,HIGH);
+    //digitalWrite(22,LOW);
+    //digitalWrite(23,HIGH);
     delay(1000);
-    digitalWrite(18,LOW);
+    digitalWrite(17,LOW);
+    //digitalWrite(23,LOW);
+    //digitalWrite(22,HIGH);
 }
 if (myData.a == 1000){
-    digitalWrite(19,HIGH);
+    digitalWrite(18,HIGH);
+    //digitalWrite(22,LOW);
+    //digitalWrite(23,HIGH);
     delay(1000);
-    digitalWrite(19,LOW);
+    digitalWrite(18,LOW);
+    //digitalWrite(23,LOW);
+    //digitalWrite(22,HIGH);
 }
 if (myData.a == 2000){
-    digitalWrite(21,HIGH);
+    digitalWrite(19,HIGH);
+    //digitalWrite(22,LOW);
+    //digitalWrite(23,HIGH);
     delay(1000);
-    digitalWrite(21,LOW);
+    digitalWrite(19,LOW);
+    //digitalWrite(23,LOW);
+    //digitalWrite(22,HIGH);    
 }
 }
  
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+  pinMode(17,OUTPUT); //500
+  pinMode(18,OUTPUT); //1000
+  pinMode(19,OUTPUT); //2000
+  //pinMode(21,OUTPUT); //RED
+  //pinMode(22,OUTPUT); //GREEN
+  //pinMode(23,OUTPUT); //BLUE
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
   Serial.println(WiFi.macAddress());  
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
+    //digitalWrite(21,HIGH);
     return;
   }
-  pinMode(18,OUTPUT); //500
-  pinMode(19,OUTPUT); //1000
-  pinMode(21,OUTPUT); //2000
-
+  else
+  {    
+    //digitalWrite(21,LOW);
+    //digitalWrite(22, HIGH);
+  } 
   esp_now_register_recv_cb(OnDataRecv);
 }
  
