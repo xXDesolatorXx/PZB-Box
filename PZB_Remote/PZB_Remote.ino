a78e8bf436e5d2e0c43ebebe3c43aa7e84743da4
xXDesolatorXx <80487483+xXDesolatorXx@users.noreply.github.com>
2023-08-08 21:14:42 +0200

    Update PZB_Remote.ino
    going from V0.1 to V0.2
    added support for and RGB LED using PIN 21,22 and 23
    making everything a bit cleaner overall

--- PZB_Remote/PZB_Remote.ino
+++ PZB_Remote/PZB_Remote.ino
@@ -1,13 +1,17 @@
 
 /*  
-PZB Remote V0.1
+PZB Remote V0.2
+
+PCB Prototype Version for Akiem Technik
 
 Made by Fabian Witzel
 */
 
 #include <esp_now.h>
 #include "WiFi.h"
+#include "credentials.h"
 
+//Define a structure for the message
 typedef struct struct_message {
   int a;
 
@@ -16,53 +20,66 @@ typedef struct struct_message {
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
+//declaring of booleans for each button
+bool C5H = false;
+bool C1K = false;
+bool C2K = false;
 
-bool C500 = false;
-bool C1000 = false;
-bool C2000 = false;
-
-volatile unsigned long sinceLastTouchC5 = 0;
-volatile unsigned long sinceLastTouchC1 = 0;
-volatile unsigned long sinceLastTouchC2 = 0;
+volatile unsigned long sinceLastTouchC5H = 0;
+volatile unsigned long sinceLastTouchC1K = 0;
+volatile unsigned long sinceLastTouchC2K = 0;
 
 bool touchDelayComp(unsigned long);
-
-uint8_t broadcastAddress[] = {0xAC, 0x67, 0xB2, 0x34, 0xA4, 0xD8};
-//AC:67:B2:34:A4:D8
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  
 }
 //500Hz Button Interrupt
 void IRAM_ATTR H500()
 {
-    C500 = true;
+    C5H = true;
 }
 //1000Hz Button Interrupt
 void IRAM_ATTR H1000()
 {
-    C1000 = true;
+    C1K = true;
 }
 //2000Hz Button Interrupt
 void IRAM_ATTR H2000()
 {  
-    C2000 = true;
+    C2K = true;
 }
 
 void setup(){
+//Setting up the Pins  
 pinMode(17,INPUT_PULLDOWN);
 pinMode(18,INPUT_PULLDOWN);
 pinMode(19,INPUT_PULLDOWN);
+pinMode(21,OUTPUT); //RED
+pinMode(22,OUTPUT); //GREEN
+pinMode(23,OUTPUT); //BLUE
+digitalWrite(21,LOW);
+digitalWrite(22,LOW);
+digitalWrite(23,LOW);
+//Setting up interrupts
 attachInterrupt(17,H500,RISING);
 attachInterrupt(18,H1000,RISING);
 attachInterrupt(19,H2000,RISING);
+//Establishing a serial connection for debugging
 Serial.begin(115200);
+//Setting the WiFi to Station Mode
 WiFi.mode(WIFI_STA);
 
 if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
-    return;
+    digitalWrite(22,LOW);
+    digitalWrite(21,HIGH);
+    return; 
+  }
+  else {
+   digitalWrite(22,HIGH);
   }
 
   esp_now_register_send_cb(OnDataSent);
@@ -78,58 +95,90 @@ if (esp_now_add_peer(&peerInfo) != ESP_OK){
 }
  
 void loop(){
-
-if (C500)
+//500Hz Loop
+if (C5H)
 {
-C500 = false;
-    if (touchDelayComp(sinceLastTouchC5))
+C5H = false;
+    if (touchDelayComp(sinceLastTouchC5H))
     {
      myData.a = 500;
       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
       if (result == ESP_OK) {
     Serial.println("Sent with success");
-  }
+    digitalWrite(21,LOW);
+    digitalWrite(22,LOW);
+    digitalWrite(23,HIGH);
+    delay(500);  
+    }
   else {
     Serial.println("Error sending the data");
+    digitalWrite(22,LOW);
+    digitalWrite(21,HIGH);
+    delay(500);
   }
-    sinceLastTouchC5 = millis();         
+    digitalWrite(21,LOW);
+    digitalWrite(23,LOW);
+    digitalWrite(22,HIGH);  
+    sinceLastTouchC5H = millis();         
     }
 }
-if(C1000){
-C1000 = false;
-    if (touchDelayComp(sinceLastTouchC1))
+//1000Hz Loop
+if(C1K){
+C1K = false;
+    if (touchDelayComp(sinceLastTouchC1K))
     {
      myData.a = 1000;
       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
       if (result == ESP_OK) {
     Serial.println("Sent with success");
+    digitalWrite(21,LOW);
+    digitalWrite(22,LOW);
+    digitalWrite(23,HIGH);
+    delay(500);    
   }
   else {
     Serial.println("Error sending the data");
+    digitalWrite(22,LOW);
+    digitalWrite(21,HIGH);
+    delay(500);    
   }
-    sinceLastTouchC1 = millis();       
+    sinceLastTouchC1K = millis(); 
+    digitalWrite(21,LOW);
+    digitalWrite(23,LOW);
+    digitalWrite(22,HIGH);          
     }
 }
-if (C2000){
-C2000 = false;
-    if (touchDelayComp(sinceLastTouchC2))
+//2000Hz Loop
+if (C2K){
+C2K = false;
+    if (touchDelayComp(sinceLastTouchC2K))
     {
      myData.a = 2000;
       esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
       if (result == ESP_OK) {
     Serial.println("Sent with success");
+    digitalWrite(22,LOW);
+    digitalWrite(23,HIGH);
+    delay(500);
   }
   else {
     Serial.println("Error sending the data");
+    digitalWrite(21,LOW);
+    digitalWrite(22,LOW);
+    digitalWrite(21,HIGH);
+    delay(500);
   } 
-    sinceLastTouchC2 = millis();        
+    digitalWrite(21,LOW);
+    digitalWrite(23,LOW);
+    digitalWrite(22,HIGH);
+    sinceLastTouchC2K = millis();        
     }
 }
 }
-
+//Delay Comparison for debouncing
 bool touchDelayComp(unsigned long lastTouch)
 {
-  if (millis() - lastTouch < 5000) return false;
+  if (millis() - lastTouch < 2000) return false;
   else
   {
   return true;
